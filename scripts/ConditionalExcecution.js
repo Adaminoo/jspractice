f6c8498429d60908473f3ae55bc2ae563ee58666
Adam Brunner <adamino@Adams-MacBook-Pro.local>
2024-09-18 08:21:26 -0600

    erm waht the files

---
+++ scripts/ConditionalExcecution.js
@@ -0,0 +1,17 @@
+let x = 4;
+let y = 6;
+let z = 10;
+
+if (true) {
+    //do this
+} else {
+    //do this
+};
+
+if (x > y) {
+    console.log("x is greater than y")
+} else if (x > z) {
+    console.log("x is greater than z")
+} else {
+    console.log("x is the smallest variable")
+};
---
+++ scripts/Operators.js
@@ -0,0 +1,34 @@
+//Arithmetic Operators
+
+let a = 4;
+let b = 3;
+
+//Addition
+let c = a + b; //c = 7
+
+//Subtraction
+let d = a - b; //d = 1
+
+//Multiplication
+let e = a * b; //e = 12
+
+//Division
+let f = a / b; //f = 1.33
+
+//Modulus Division (Returns remainer)
+let g = a % b; //g = 1
+
+//Increment
+let h = ++a; //h = 5
+
+//Decrement
+let i = --b //i = 2
+
+
+
+//Ternary operator example
+let y = 6;
+let z = 4;
+
+//if a > b, return yes. If not, return no
+a > b ? "yes" : "no";
