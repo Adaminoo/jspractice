54f700e4383aee3a58fbb5cb6c545cbc4caa2553
Adam Brunner <adamino@Adams-MacBook-Pro.local>
2024-09-25 07:36:59 -0600

    erm

--- scripts/interator.js
+++ scripts/interator.js
@@ -1,3 +1,4 @@
+// Basic Iterator Syntax
 const str = "Hello World";
 
 const iterator = str[Symbol.iterator]();
@@ -7,4 +8,30 @@ let character = iterator.next();
 while (!character.done && character.value !== " ") {
     console.log(character.value);
     character = iterator.next();
+}
+
+
+//erm cant remember what this is but it seems important
+const obj = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+
+const newObj = {
+    ...obj,
+    age: 31,
+}
+
+const slideString = "erm wtf ";
+
+const slideIterator = slideString[Symbol.iterator]();
+
+let slide = slideIterator.next();
+
+function ermwtf() {
+    while (!slide.done && slide.value !== " ") {
+        console.log(slide.value);
+        slide = slideIterator.next();
+    }
 }
