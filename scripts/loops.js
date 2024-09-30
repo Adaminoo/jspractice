1bbc23d7b95c871446ab374d67551960b34e3186
Adam Brunner <adamino@Adams-MacBook-Pro.local>
2024-09-30 07:36:23 -0600

    erm

---
+++ scripts/loops.js
@@ -0,0 +1,20 @@
+
+//for of example
+let shoppingList2 = [
+    {name: 'bananas', count: 4},
+    {name: 'bread', count: 2},
+    {name: 'eggs', count: 12},
+    {name: 'sugar', count: 2}
+]
+
+let totalItems2 = 0;
+
+for (let item of shoppingList2) {
+    totalItems2 = totalItems2 + item.count;
+}
+
+console.log('for of example: ' + totalItems2);
+
+let loopPractice = [
+    
+]
